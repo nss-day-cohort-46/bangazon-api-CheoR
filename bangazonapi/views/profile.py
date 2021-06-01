5894845558041c3db9ac82415012e743034124ea
CheoR <cheo.roman+github@gmail.com>
2021-06-01 14:07:33 -0500

    fix: closes #22 - now returns correct user profile when client submits a GET request to /profile.

--- bangazonapi/views/profile.py
+++ bangazonapi/views/profile.py
@@ -81,8 +81,9 @@ class Profile(ViewSet):
             }
         """
         try:
-            current_user = Customer.objects.get(user=4)
-            current_user.recommends = Recommendation.objects.filter(recommender=current_user)
+            current_user = Customer.objects.get(user=request.auth.user)
+            current_user.recommends = Recommendation.objects.filter(
+                recommender=current_user)
 
             serializer = ProfileSerializer(
                 current_user, many=False, context={'request': request})

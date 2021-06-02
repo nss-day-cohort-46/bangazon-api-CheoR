d4a3b9eaddecb68c4d2062f2996fd88ba40bddea
CheoR <cheo.roman+github@gmail.com>
2021-06-02 14:05:53 -0500

    build: use python functions and SQL queries to receive and respond to client request.

---
+++ bangazonareports/views/products/inexpensiveProducts.py
@@ -0,0 +1,47 @@
+"""Module for generating report of products $999 or less"""
+
+
+from levelupreports.views import Connection
+from levelupapi.models import Game
+from django.shortcuts import render
+import sqlite3
+
+from bangazonapi.models import Product
+from bangazonareports.views import Connection
+
+
+def inexpensive_products_list(request):
+    """Function builds an HTML report of products $999 or less"""
+    if request.method == 'GET':
+        with sqlite3.connect(Connection.absolute_path_to_db) as conn:
+            conn.row_factory = sqlite3.Row
+            db_cursor = conn.cursor()
+
+            db_cursor.execute("""
+                SELECT
+                    p.name,
+                    p.price,
+                    p.description,
+                    p.quantity
+                FROM bangazonapi_product p
+                WHERE price <= 999
+                ORDER BY price DESC;
+            """)
+
+            dataset = db_cursor.fetchall()
+
+            products = []
+
+            for row in dataset:
+                products.appned({
+                    "id": row["id"],
+                    "name": row["name"],
+                    "description": row["decriptoin"],
+                    "quantity": row["quantity"]
+                })
+        template = 'products/list_inexepensive_products.html'
+        context = {
+            'products': products
+        }
+
+        return render(request, template, context)
